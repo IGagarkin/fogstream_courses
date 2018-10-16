4c7a72c7739700aff404fd58dbec6a2ada783977
Gagarkin Ivan <i.gagarkin@bk.ru>
2018-10-16 14:43:59 +0300

    Initial commit.

--- Practice_1/task_5.py
+++ Practice_1/task_5.py
@@ -1,14 +1,14 @@
 
 """
-.С начала суток прошло H часов, M минут, S секунд (0 ≤ H < 12, 0 ≤ M < 60, 0 ≤ S < 60). По данным числам H, M, S
+С начала суток прошло H часов, M минут, S секунд (0 ≤ H < 12, 0 ≤ M < 60, 0 ≤ S < 60). По данным числам H, M, S
  определите угол (в градусах), на который повернулаcь часовая стрелка с начала суток и выведите его в виде 
 действительного числа.
 """
 
-H=8
-M=49
-S=56
-#H = int(input("Number of lesson: "))
+H = int(input("H: "))
+M = int(input("M: "))
+S = int(input("S: "))
+
 
 ONE_DEGREE=(12*3600+60*60+60)/360
 ANG_H=360/12
@@ -17,8 +17,8 @@ ANG_S=360/(3600*12)
 
 
 SUM_ANG=(H*ANG_H)+(M*ANG_M)+(S*ANG_S)
-print ("Angle: "+str(SUM_ANG))
-print ("Round angle: "+str(round(SUM_ANG)))
+
+print ("Rounded angle: "+str(round(SUM_ANG)))
 
 
 
